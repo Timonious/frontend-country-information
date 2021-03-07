06615adafde481ffd4c65e8ba5fab8f4b42caa08
Your Name <timon_a@hotmail.com>
2021-03-07 11:28:39 +0100

    Cleaned code, added try and catch and loading gif to regions page

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-// const axios = require("axios");
-const button = document.getElementById("button");
-button.addEventListener("click", function () {
-    country = inputField.value;
-    searchCountry();
-});
-const inputField = document.getElementById("input");
-const input = (e) => {
-    country = e.target.value;
-    if (e.key === 'Enter') {
-        searchCountry();
-    }
-}
-inputField.addEventListener('keyup', input);
-const content = document.getElementById('content');
 let country = "";
+
 async function searchCountry() {
     const oldFlag = document.getElementById('flag-image'),
         exCountryNameH2 = document.getElementById('country-name-header'),
@@ -30,55 +16,53 @@ async function searchCountry() {
     try {
         errorMsg.setAttribute('class', 'hide');
         worldLoader.removeAttribute('class');
-        const response = await axios.get(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`)
-        //     data =
-        const { currencies, capital, flag, subregion, languages, population, name} = response.data[0];
-        // console.log(data);
-        const capitalAndMoney = () => {
-            let sum = ``;
-            switch (true) {
-                case currencies.length === 1:
-                    return `The capital is ${capital} and you can pay with ${currencies[0].name}'s.`;
-                case currencies.length === 2:
-                    return `The capital is ${capital} and you can pay with ${currencies[0].name}'s and ${currencies[1].name}'s.`;
-                case currencies.length > 2:
-                    for (let i = 1; i < currencies.length - 2; i++) {
-                        sum += currencies[i].name + `, `;
-                    }
-                    return `The capital is ${capital} and you can pay with ${currencies[0].name}, ${sum} ${currencies[currencies.length - 2].name} and ${currencies[currencies.length - 1].name}.`;
-                default :
-                    return `I can't seem to retrieve the capital nor the currencies`
-            }
-        }
-        function spokenLanguages() {
-            let sum = ``;
-            if (languages.length === 1) {
-                return `They speak ${languages[0].name}.`;
-            }
-            if (languages.length === 2) {
-                return `They speak ${languages[0].name} and ${languages[languages.length - 1].name}.`;
-            }
-            if (languages.length > 2) {
-                for (let i = 1; i < languages.length - 2; i++) {
-                    sum += languages[i].name + `, `;
+        const response = await axios.get(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`),
+            {currencies, capital, flag, subregion, languages, population, name} = response.data[0],
+            capitalAndMoney = () => {
+                let sum = ``;
+                switch (true) {
+                    case currencies.length === 1:
+                        return `${currencies[0].name}'s.`;
+                    case currencies.length === 2:
+                        return `${currencies[0].name}'s and ${currencies[1].name}'s.`;
+                    case currencies.length > 2:
+                        for (let i = 1; i < currencies.length - 2; i++) {
+                            sum += currencies[i].name + `, `;
+                        }
+                        return `${currencies[0].name}, ${sum} ${currencies[currencies.length - 2].name} and ${currencies[currencies.length - 1].name}.`;
+                    default :
+                        return ` nothing, I guess they started a resource-based economy here, good for them!`
                 }
-            }
-            return `They speak ${languages[0].name},${sum} ${languages[languages.length - 2].name} and ${languages[languages.length - 1].name}.`;
-        }
-
-        const flagImg = document.createElement('img');
+            },
+            spokenLanguages = () => {
+                let sum = ``;
+                switch (true) {
+                    case languages.length === 1:
+                        return `${languages[0].name}.`;
+                    case languages.length === 2:
+                        return `${languages[0].name} and ${languages[languages.length - 1].name}.`;
+                    case languages.length > 2:
+                        for (let i = 1; i < languages.length - 2; i++) {
+                            sum += languages[i].name + `, `;
+                        }
+                        return `${languages[0].name},${sum} ${languages[languages.length - 2].name} and ${languages[languages.length - 1].name}.`;
+                    default:
+                        return `I guess they don't speak in ${name}, sad.`
+                }
+            },
+            flagImg = document.createElement('img'),
+            countryNameH2 = document.createElement('div'),
+            textContent = document.createElement('div');
         flagImg.src = flag;
         flagImg.setAttribute('id', 'flag-image');
         content.appendChild(flagImg);
-        const countryNameH2 = document.createElement('div');
         countryNameH2.setAttribute('id', 'country-name-header');
         countryNameH2.innerText = `${name}`;
         content.appendChild(countryNameH2);
-        const textContent = document.createElement('div');
         textContent.innerText =
             `${name} is situated in ${subregion}. It has a population of ${numeral(population).format('0,0')} people.
-             ${capitalAndMoney()}
-             ${spokenLanguages()}`;
+             The capital is ${capital} and you can pay with ${capitalAndMoney()}
+             They speak ${spokenLanguages()}`;
         textContent.setAttribute('class', 'information');
         content.appendChild(textContent);
     } catch (e) {
@@ -93,5 +77,19 @@ async function searchCountry() {
     worldLoader.setAttribute('class', 'hide')
 }
 
+button = document.getElementById("button");
+button.addEventListener("click", function () {
+    country = inputField.value;
+    searchCountry();
+});
+const inputField = document.getElementById("input"),
+    input = (e) => {
+        country = e.target.value;
+        if (e.key === 'Enter') {
+            searchCountry();
+        }
+    }
+inputField.addEventListener('keyup', input);
+const content = document.getElementById('content');
 
 
--- region.js
+++ region.js
@@ -1,77 +1,86 @@
+const worldLoader = document.getElementById('loading'),
+    listFrame = document.getElementById('country-list-div'),
+    list = document.getElementById('country-list');
+worldLoader.removeAttribute('class');
 async function searchAllTheCountries() {
-    const response = await axios.get('https://restcountries.eu/rest/v2/all')
-    const data = response.data
-    const sortByPopulation = data.sort((a, b) => a.population - b.population)
-    const nameAndFlag = () => {
-        const list = document.getElementById('country-list')
-        for (let i = 0; i < data.length; i++) {
-            const listItem = document.createElement('li')
-            listItem.setAttribute('class', 'list-item')
-            list.appendChild(listItem)
-            const flag = document.createElement('img');
-            flag.src = data[i].flag;
-            flag.setAttribute('class', 'flag-icon');
-            listItem.appendChild(flag);
-            const countryName = document.createElement('div');
-            const colorCoder = (i) => {
-                const region = data[i].region.toString(), subregion = data[i].subregion;
-                switch (true) {
-                    case subregion === "Central America":
-                        return countryName.setAttribute('class', 'north-america')
-                    case subregion === "Northern America":
-                        return countryName.setAttribute('class', 'north-america')
-                    case subregion === "South America":
-                        return countryName.setAttribute('class', 'south-america')
-                    case region === "Oceania":
-                        return countryName.setAttribute('class', 'oceania')
-                    case region === "Africa":
-                        return countryName.setAttribute('class', 'africa')
-                    case region === "Europe":
-                        return countryName.setAttribute('class', 'europe')
-                    case region === "Asia":
-                        return countryName.setAttribute('class', 'asia')
-                    case region === "Polar":
-                        return countryName.setAttribute('class', 'antarctic')
-                    default:
-                        return countryName.setAttribute('class', 'no-continent')
+    const response = await axios.get('https://restcountries.eu/rest/v2/all'),
+        data = response.data,
+        errorMsg = document.getElementById('error'),
+        sortByPopulation = data.sort((a, b) => a.population - b.population);
+    try {
+        errorMsg.setAttribute('class', 'hide');
+        const nameAndFlag = () => {
+            for (let i = 0; i < data.length; i++) {
+                const listItem = document.createElement('li'), flag = document.createElement('img'),
+                    countryName = document.createElement('div');
+                listItem.setAttribute('class', 'list-item');
+                list.appendChild(listItem);
+                flag.src = data[i].flag;
+                flag.setAttribute('class', 'flag-icon');
+                listItem.appendChild(flag);
+                const colorCoder = (i) => {
+                    const region = data[i].region, subregion = data[i].subregion;
+                    switch (true) {
+                        case subregion === "Central America":
+                            return countryName.setAttribute('class', 'north-america')
+                        case subregion === "Northern America":
+                            return countryName.setAttribute('class', 'north-america')
+                        case subregion === "South America":
+                            return countryName.setAttribute('class', 'south-america')
+                        case region === "Oceania":
+                            return countryName.setAttribute('class', 'oceania')
+                        case region === "Africa":
+                            return countryName.setAttribute('class', 'africa')
+                        case region === "Europe":
+                            return countryName.setAttribute('class', 'europe')
+                        case region === "Asia":
+                            return countryName.setAttribute('class', 'asia')
+                        case region === "Polar":
+                            return countryName.setAttribute('class', 'antarctic')
+                        default:
+                            return countryName.setAttribute('class', 'no-continent')
+                    }
                 }
+                colorCoder([i])
+                countryName.innerText = `${data[i].name}`;
+                countryName.setAttribute('id', 'list' + [i])
+                listItem.appendChild(countryName)
+                const listId = document.getElementById('list' + [i]),
+                    hiddenPopulation = document.createElement('div'),
+                    population = `Population: ${numeral(data[i].population).format('0,0')}`
+                hiddenPopulation.setAttribute('class', 'hidden-pop')
+                hiddenPopulation.setAttribute('id', 'hide-pop' + [i])
+                hiddenPopulation.innerText = population
+                listItem.appendChild(hiddenPopulation)
+                listId.addEventListener('click', function () {
+                    const hiddenPop = document.getElementById('hide-pop' + [i])
+                    if (hiddenPop) {
+                        listItem.removeChild(hiddenPop)
+                        const shownPopulation = document.createElement('div')
+                        shownPopulation.setAttribute('class', 'shown-pop')
+                        shownPopulation.setAttribute('id', 'show-pop' + [i])
+                        shownPopulation.innerText = population
+                        return listItem.appendChild(shownPopulation)
+                    }
+                    const shownPopulation = document.getElementById('show-pop' + [i])
+                    if (shownPopulation) {
+                        listItem.removeChild(shownPopulation)
+                        const hiddenPopulation = document.createElement('div')
+                        hiddenPopulation.setAttribute('class', 'hidden-pop')
+                        hiddenPopulation.setAttribute('id', 'hide-pop' + [i])
+                        hiddenPopulation.innerText = population
+                        return listItem.appendChild(hiddenPopulation)
+                    }
+                });
             }
-            colorCoder([i])
-            countryName.innerText = `${data[i].name}`;
-            countryName.setAttribute('id', 'list' + [i])
-            listItem.appendChild(countryName)
-            const listId = document.getElementById('list' + [i])
-            const hiddenPopulation = document.createElement('div')
-            hiddenPopulation.setAttribute('class', 'hidden-pop')
-            hiddenPopulation.setAttribute('id', 'hide-pop' + [i])
-            const population = `Population: ${numeral(data[i].population).format('0,0')}`
-            hiddenPopulation.innerText = population
-            listItem.appendChild(hiddenPopulation)
-            listId.addEventListener('click', function () {
-                const hiddenPop = document.getElementById('hide-pop' + [i])
-                if (hiddenPop) {
-                    listItem.removeChild(hiddenPop)
-                    const shownPopulation = document.createElement('div')
-                    shownPopulation.setAttribute('class', 'shown-pop')
-                    shownPopulation.setAttribute('id', 'show-pop' + [i])
-                    shownPopulation.innerText = population
-                    return listItem.appendChild(shownPopulation)
-
-                }
-                const shownPopulation = document.getElementById('show-pop' + [i])
-                if (shownPopulation) {
-                    listItem.removeChild(shownPopulation)
-                    const hiddenPopulation = document.createElement('div')
-                    hiddenPopulation.setAttribute('class', 'hidden-pop')
-                    hiddenPopulation.setAttribute('id', 'hide-pop' + [i])
-                    hiddenPopulation.innerText = population
-                    return listItem.appendChild(hiddenPopulation)
-
-                }
-            });
         }
+        nameAndFlag()
+    } catch (e) {
+        console.error(e);
+        errorMsg.removeAttribute('class')
+        errorMsg.textContent = `Something went wrong retrieving ALL THE COUNTRIES! please try again later.`;
     }
-    nameAndFlag()
+    worldLoader.setAttribute('class', 'hide')
+    listFrame.removeAttribute("class")
 }
-
 searchAllTheCountries()
